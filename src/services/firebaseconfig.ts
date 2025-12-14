f325b0d9845489836857756609c6c667d1bfbf93
mithunpagadwar8 <mithunpagadwar8@gmail.com>
2025-12-15 04:09:12 +0530

    Update firebaseconfig.ts

--- src/services/firebaseconfig.ts
+++ src/services/firebaseconfig.ts
@@ -10,12 +10,12 @@ import { getStorage } from "firebase/storage";
  */
 
 const firebaseConfig = {
-  apiKey: import.meta.env.VITE_FIREBASE_API_KEY || "",
-  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN || "",
-  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID || "",
-  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET || "",
-  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID || "",
-  appId: import.meta.env.VITE_FIREBASE_APP_ID || "",
+  VITE_FIREBASE_API_KEY=AIzaSyBkiKQBrYNz9YVR56waGOtDbXtQSl8Jl7I
+VITE_FIREBASE_AUTH_DOMAIN=grampanchayatchikhali-8a314.firebaseapp.com
+VITE_FIREBASE_PROJECT_ID=grampanchayatchikhali-8a314
+VITE_FIREBASE_STORAGE_BUCKET=grampanchayatchikhali-8a314.firebasestorage.com
+VITE_FIREBASE_MESSAGING_SENDER_ID=627147233902
+VITE_FIREBASE_APP_ID=1:627147233902:web:473219f2d4c2630da3a47f
 };
 
 export const isConfigured = () =>
